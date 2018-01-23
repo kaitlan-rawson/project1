e378047f21a1a61d0d71c7a58701a4b07cf2f1b2
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-01-22 20:04:13 -0700

    commit5

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const express = require ('express'),
     bodyParser = require('body-parser'),
     app = express();
     port = 3001;
-    ctrl = require('./controllers/controllers.js')
+    ctrl = require('./controllers/controllers.js');
+    cors = require('cors')
 
 app.use(bodyParser.json())
-
+app.use(cors());
 app.get('/api/travels',ctrl.get)
 app.post('/api/travels',ctrl.post)
 app.put('/api/travels/:id',ctrl.put)
